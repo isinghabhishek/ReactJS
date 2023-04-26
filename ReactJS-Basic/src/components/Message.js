9bed40eb8e51c53abd15776a6c5954e22ced9d0c
isinghabhishek <singhabhishek3006.abhi@gmail.com>
2023-04-26 16:43:17 +0530

    Update: react basic files

---
+++ ReactJS-Basic/src/components/Message.js
@@ -0,0 +1,29 @@
+// 10). State 
+
+import React, { Component } from "react";
+
+class Message extends Component {
+    constructor() {
+        super()
+        this.state = {
+            message: 'Welcome Visitor'
+        }
+    };
+
+    changeMessage() {
+        this.setState({
+            message: 'Thank you for subscribing'
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1> {this.state.message} </h1>
+                <button onClick={() => this.changeMessage()} >Subscribe</button>
+            </div>
+        )
+    };
+};
+
+export default Message
