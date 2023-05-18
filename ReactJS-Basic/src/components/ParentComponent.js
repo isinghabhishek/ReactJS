0cc70f417a414818b392d1629e51a92ea980f52d
isinghabhishek <singhabhishek3006.abhi@gmail.com>
2023-05-18 22:20:58 +0530

    add: file in the react basic

---
+++ ReactJS-Basic/src/components/ParentComponent.js
@@ -0,0 +1,35 @@
+// 15). Methods as props
+// in the parent component defind the method, on the child component 
+// tag pass the method as a props in the child component 
+
+import React, { Component } from 'react'
+import ChildComponent from './ChildComponent'
+
+class ParentComponent extends Component {
+
+constructor(props) {
+  super(props)
+
+  this.state = {
+     parentName: 'Parent'
+  }
+  this.greetParent = this.greetParent.bind(this)
+}
+
+// methods
+greetParent(childName) {
+  // template literals
+  alert(`Hello ${this.state.parentName} from ${childName}`)
+} 
+// BackTactics(``)
+
+  render() {
+    return (
+      <div>
+        <ChildComponent greetHandler={this.greetParent}/>
+      </div>
+    )
+  }
+}
+
+export default ParentComponent
