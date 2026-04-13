abdbb3b120c543c93727b3991da9f598fadf717a
isinghabhishek <singhabhishek3006.abhi@gmail.com>
2026-04-13 22:12:23 +0530

    Update app tests for current UI

--- ReactJS-Basic/src/App.test.js
+++ ReactJS-Basic/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the post form submit button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const submitButton = screen.getByRole('button', { name: /submit/i });
+  expect(submitButton).toBeInTheDocument();
 });
